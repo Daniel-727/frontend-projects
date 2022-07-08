ca6b67114dbb750557efd08f0f2170e38d233d4f
Daniel Li <zaoster777@gmail.com>
2022-07-08 12:50:18 -0500

    added reset functonality and made number formatting prettier

--- investment-tracker/index.js
+++ investment-tracker/index.js
@@ -1,19 +1,20 @@
 //selecting elements
-let investment;
-let interest;
-let years;
+let investment_field = document.getElementById("invested");
+let interest_field = document.getElementById("interest");
+let years_field = document.getElementById("years");
 const submit_btn = document.querySelector("#submit");
+const reset_btn = document.querySelector("#reset");
 let return_value = document.querySelector(".return_value");
 const form = document.querySelector("form");
 
 //submit button event listener
-submit_btn.addEventListener("click", submit());
+submit_btn.addEventListener("click", submit); //remove the parentheses if you don't want the function to be called when the page loads
 
 //submit function
 function submit() {
-    investment = parseInt(document.getElementById("invested").value, 10);
-    interest = parseInt(document.getElementById("interest").value, 10) * 0.01;
-    years = parseInt(document.getElementById("years").value, 10);
+    investment = parseInt(investment_field.value, 10);
+    interest = parseInt(interest_field.value, 10) * 0.01;
+    years = parseInt(years_field.value, 10);
     calculate(investment, interest, years);
 }
 
@@ -33,11 +34,26 @@ function calculate(invested, interest, years) {
         total = total + total * interest;
     }
     console.log(total); */
-    return_value.textContent = `Your total investment after ${years} years will be ${total} dollars.`;
+
+    //formatting output to make it more presentable
+    //let two_decimal = total.toFixed(2); toFixed lets us round to x decimal place, but isn't required as toLocaleString has options to do that for us
+    let formatted_total = total.toLocaleString("en-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+    return_value.textContent = `Your total investment after ${years} years will be $${formatted_total} dollars.`;
 }
 
 form.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         submit();
     }
+});
+
+//reset button event listener
+reset_btn.addEventListener("click", () => {
+    investment_field.value = "";
+    interest_field.value = "5%";
+    years_field.value = "";
+    return_value.textContent = "";
 });
