9d14dea269978da6182ea2c74706e151a99dd3da
Daniel Li <zaoster777@gmail.com>
2022-07-12 14:45:41 -0500

    finished functionality and styling of basic weather app

--- weather-tracker/index.js
+++ weather-tracker/index.js
@@ -4,9 +4,13 @@ const search = document.querySelector(".search-bar").querySelector("input");
 
 const temperature = document.querySelector(".temperature");
 const weather_condition = document.querySelector(".weather-condition");
-const day_night = document.querySelector(".day-night");
+const current_time = document.querySelector(".current-time");
 const weather_icon = document.querySelector(".weather-icon");
 const search_btn = document.querySelector('.search-btn');
+
+
+const results = document.querySelector('.results');
+const title = results.querySelector('h1');
 
 // Search bar placeholder functionality
 search.addEventListener("focus", () => {
@@ -28,13 +32,13 @@ const query_results = () => {
     url: 'https://community-open-weather-map.p.rapidapi.com/weather',
     params: {
         q: query,
-        lat: '0',
-        lon: '0',
-        callback: 'result',
-        id: '2172797',
-        lang: 'null',
+        lat: '',
+        lon: '',
+        callback: '',
+        id: '',
+        lang: '',
         units: 'imperial',
-        mode: 'xml'
+        mode: 'json'
     },
     headers: {
         'X-RapidAPI-Key': '',
@@ -50,19 +54,63 @@ const query_results = () => {
     });
 }
 
-/*response.data will have main which will have temp it will also have weather which has an icon and main it will also have dt which might be the time?*/
+/*response.data will have main which will have temp it will also have weather which has an icon and main it will also have dt which is the unix timestamp*/
 
 const print_results = (data) => {
+    temperature.textContent = `${data.main.temp}\u2109`;
+    weather_condition.textContent = data.weather[0].main;
+    
+    const utc_seconds = data.dt;
+    const timezone_offset = data.timezone;
+    const time_object = calcTime(utc_seconds, timezone_offset);
 
+    current_time.textContent = `Local Time: ${time_object.converted_hours}:${time_object.minutes} ${time_object.day_night}`;
+
+    title.textContent = search.value;
+
+    results.classList.remove('hidden');
 }
 
-const test = () => {
+/* const test = () => {
     console.log(search.value);
-}
+} */
 
+//calculate if it is daytime or nighttime
+
+const calcTime = (unix, timezone) => {
+    
+    let dateObj = new Date((unix + timezone) * 1000);
+    let day_night = 'am';
+    
+    //make our clock display 12 and not 0 
+    let hours = dateObj.getUTCHours() % 12;
+    if (hours === 0){
+        hours = 12;
+    }
+
+    //keep track of am or pm
+    if (dateObj.getUTCHours() <= 12){
+
+    } else {
+        day_night = 'pm';
+    }
+
+    //formatting our clock to look nice
+    let converted_hours = hours.toString().padStart(2, '0');
+    let minutes = dateObj.getUTCMinutes().toString().padStart(2,'0');
+
+    
+    return {converted_hours,minutes,day_night};
+}
 
 //Search button & enter event
+const body = document.querySelector('body'); 
 
+body.addEventListener('keypress', e => {
+    if(e.key === 'Enter'){
+        query_results();
+    }
+});
 search_btn.addEventListener('click', query_results);
 
 
