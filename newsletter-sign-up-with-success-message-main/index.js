dff040b5527696220f9941c1beb1ff822c811e59
Daniel Li <zaoster777@gmail.com>
2023-08-12 15:31:47 -0500

    newsletter add form validation functionality

---
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,28 @@
+const email_regex = /[\w /.]+@\w+.com/;
+const form = document.querySelector('[data-id="form"]');
+const email_input = document.querySelector('[data-id="email_input"]');
+
+const error_message = document.querySelector('[data-id="error"]');
+
+const validEmail = () => {
+  let result = email_regex.test(email_input.value);
+
+  if (result) {
+    email_input.classList.remove("input_error");
+    error_message.classList.add("hidden");
+    return true;
+  } else {
+    email_input.classList.add("input_error");
+    error_message.classList.remove("hidden");
+    return false;
+  }
+};
+
+email_input.addEventListener("change", () => validEmail());
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (validEmail()) {
+    form.submit();
+  }
+});
